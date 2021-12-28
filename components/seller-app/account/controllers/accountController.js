46ae6ef061018f48668109bf2a94ebdafdb7ee93
Hoang Truong <hoangtruongvann@gmail.com>
2021-12-28 21:56:08 +0700

    update product

--- components/seller-app/account/controllers/accountController.js
+++ components/seller-app/account/controllers/accountController.js
@@ -1,4 +1,9 @@
+const async = require("hbs/lib/async");
+const accountService = require('../services/accountService')
 
-exports.profile = (req,res,next) =>{
-    res.render('../components/seller-app/account/views/profile',{layout: 'sellerLayout.hbs'});
+exports.profile =  async (req,res,next) =>{
+    const user = req.user;
+    const shop_id = user.user_shop;
+    const shop = await accountService.getShop(shop_id);
+    res.render('../components/seller-app/account/views/profile',{layout: 'sellerLayout.hbs',shop});
 }
