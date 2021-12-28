fd719f54f19df04355e2e66599da7ccd39c30d60
Tr-nT-nL-c <66482009+GaLaTiCosS@users.noreply.github.com>
2021-12-28 15:23:47 +0700

    login

--- components/user-app/product/controllers/productController.js
+++ components/user-app/product/controllers/productController.js
@@ -1,10 +1,16 @@
+const productM = require('../services/products')
 
-exports.list = (req,res,next)=>
+
+exports.list = async (req,res,next)=>
 {
-    res.render('../components/user-app/product/views/productList')
+    let products = await productM.getAll(page = req.query.page, itemPerPage = 6);
+    products.forEach(value => {
+        value.image_link = value['images.image_link']
+    });
+    res.render('../components/user-app/product/views/productList',{layout: 'userLayout', products:products, total:products.length})
 }
 
 exports.detail = (req,res,next)=>
 {
-    res.render('../components/user-app/product/views/productDetail')
+    res.render('../components/user-app/product/views/productDetail',{layout: 'userLayout'})
 }
