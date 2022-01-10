f714d34e9be214db78dd93e86d0451c7b129e8f5
Hoang Truong <hoangtruongvann@gmail.com>
2022-01-10 16:57:50 +0700

    fix detail order data display

--- components/seller-app/orders/services/ordersDetail.js
+++ components/seller-app/orders/services/ordersDetail.js
@@ -6,21 +6,40 @@ exports.getAll = () => {
 }
 exports.getAllByOrderId = (id) => {
     return models.detailorders.findAll({
-        where: {
-            order_id: id
-        },
-        raw: true
-    })
-}
-exports.getAllProduct = (id) => {
-    return models.detailorders.findAll({
-        include: [{
-            model: models.products,
-            as: "product"
+        include:[{
+            model:models.products,
+            as:'product',
+            include:[
+                {
+                model:models.images,
+                as:'images',
+                where:{image_stt:1}
+                },
+                {
+                model:models.categories,
+                as:'category',
+                },
+                {
+                    model:models.brands,
+                    as:'brand'
+                }
+            ]
         }],
-        where: {
-            product_id: id
+        where:{
+            order_id:id
         },
-        raw: true
+        raw:true
     })
 }
+// exports.getAllProduct = (id) => {
+//     return models.detailorders.findAll({
+//         include: [{
+//             model: models.products,
+//             as: "product"
+//         }],
+//         where: {
+//             product_id: id
+//         },
+//         raw: true
+//     })
+// }
