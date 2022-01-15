5f2cb8ccf636d41228e90189dc3ea4863c66d631
anhminh <nguyenanhminh04012001@gmail.com>
2022-01-15 17:03:17 +0700

    fix review, rating

--- components/user-app/product/controllers/productController.js
+++ components/user-app/product/controllers/productController.js
@@ -2,23 +2,22 @@ const productM = require('../services/products')
 const categoryM = require('../services/category');
 
 
-exports.list = async (req,res,next)=>
-{
+exports.list = async (req, res, next) => {
     let itemsPerPage = 6;
     let currPage = req.query.page ? req.query.page : 1;
 
 
     let categories = await categoryM.getAll();
-	for(let i in categories) {
-		categories[i] = categories[i].dataValues
-	}
-	for(let i =0;i< categories.length;i++) {
-		if (categories[i].parent_category){
-			categories.splice(i, 1)
-			i--;
-		}
-	}
-    if (!req.query.page){
+    for (let i in categories) {
+        categories[i] = categories[i].dataValues
+    }
+    for (let i = 0; i < categories.length; i++) {
+        if (categories[i].parent_category) {
+            categories.splice(i, 1)
+            i--;
+        }
+    }
+    if (!req.query.page) {
         req.query.page = 1;
     }
     let products = await productM.getFull();
@@ -32,25 +31,25 @@ exports.list = async (req,res,next)=>
     let pageList = [];
     pages = Math.ceil(products.length / itemsPerPage) == 0 ? 1 : Math.ceil(products.length / itemsPerPage);
 
-    for (let i = 1; i <= pages; i++){
-        pageList.push({num: i});
+    for (let i = 1; i <= pages; i++) {
+        pageList.push({ num: i });
     }
 
     pageList[currPage - 1].active = 1;
 
     products = products.slice((currPage - 1) * itemsPerPage, currPage * itemsPerPage);
 
-    
-    let first = {}; let last =  {};
+
+    let first = {}; let last = {};
     first.page = 1;
     last.page = pages;
-    if (currPage == pages){
+    if (currPage == pages) {
         last.state = 'disabled';
-    }else last.state = null;
-    if (currPage == 1){
+    } else last.state = null;
+    if (currPage == 1) {
         first.state = 'disabled';
-    }else first.state = null;
-    res.render('../components/user-app/product/views/productList',{layout: 'userLayout', products:products,categories:categories,page:pageList, first:first, last:last})
+    } else first.state = null;
+    res.render('../components/user-app/product/views/productList', { layout: 'userLayout', products: products, categories: categories, page: pageList, first: first, last: last })
 }
 
 exports.detail = async (req, res, next) => {
@@ -63,23 +62,21 @@ exports.detail = async (req, res, next) => {
 
     let images = await productM.getImagesProduct(req.params.product_id);
     let shop = await productM.getShop(req.params.product_id);
-    let relatedProducts = await productM.getRelatedProducts(product.product_id, product.category_id); 
+    let relatedProducts = await productM.getRelatedProducts(product.product_id, product.category_id);
     if (relatedProducts.length > 4) {
         relatedProducts = relatedProducts.slice(0, 5);
-    } 
+    }
     let user = req.user;
     let reviews = await productM.getReviews(req.params.product_id);
-    
-    console.log("🚀 ~ file: productController.js ~ line 38 ~ exports.detail= ~ relatedProducts", reviews)
-    
+    reviews.reverse();
     res.render('../components/user-app/product/views/productDetail', { layout: 'userLayout', product: product, images: images, shop: shop, relatedProducts, user, reviews })
 }
 
-exports.fetching = async function (req, res, next){
+exports.fetching = async function (req, res, next) {
     let key = req.query.key;
     let products = await productM.getAllFetch();
     let response = [];
-    products.forEach(value =>{
+    products.forEach(value => {
         if (value.dataValues.product_name.toLowerCase().includes(key.toLowerCase())) {
             response.push(value.dataValues);
         }
@@ -88,24 +85,25 @@ exports.fetching = async function (req, res, next){
     res.json(response);
 }
 
-exports.search = async function (req, res, next){
+exports.search = async function (req, res, next) {
     let categories = await categoryM.getAll();
-	for(let i in categories) {
-		categories[i] = categories[i].dataValues
-	}
-	for(let i =0;i< categories.length;i++) {
-		if (categories[i].parent_category){
-			categories.splice(i, 1)
-			i--;
-		}
-	}
+    for (let i in categories) {
+        categories[i] = categories[i].dataValues
+    }
+    for (let i = 0; i < categories.length; i++) {
+        if (categories[i].parent_category) {
+            categories.splice(i, 1)
+            i--;
+        }
+    }
     let key = req.query.key;
     let products = await productM.getFull();
     let response = [];
-    products.forEach(value =>{
+    products.forEach(value => {
         if (value.product_name.toLowerCase().includes(key.toLowerCase())) {
             response.push(value);
-        }})
+        }
+    })
     response.forEach(value => {
         value.image_link = value['images.image_link']
     });
@@ -117,38 +115,39 @@ exports.search = async function (req, res, next){
     let pageList = [];
     pages = Math.ceil(response.length / itemsPerPage) == 0 ? 1 : Math.ceil(response.length / itemsPerPage);
 
-    for (let i = 1; i <= pages; i++){
-        pageList.push({num: i});
+    for (let i = 1; i <= pages; i++) {
+        pageList.push({ num: i });
     }
 
     pageList[currPage - 1].active = 1;
 
     response = response.slice((currPage - 1) * itemsPerPage, currPage * itemsPerPage);
 
-    
-    let first = {}; let last =  {};
+
+    let first = {}; let last = {};
     first.page = 1;
     last.page = pages;
-    if (currPage == pages){
+    if (currPage == pages) {
         last.state = 'disabled';
     }
-    if (currPage == 1){
+    if (currPage == 1) {
         first.state = 'disabled';
     }
-    res.render('../components/user-app/product/views/productSearch',{layout: 'userLayout', products:response,categories:categories,
-page:pageList, first:first, last:last, key:{name:key}});
+    res.render('../components/user-app/product/views/productSearch', {
+        layout: 'userLayout', products: response, categories: categories,
+        page: pageList, first: first, last: last, key: { name: key }
+    });
     // res.json(response)
 }
- 
+
 exports.addReivew = async function (req, res, next) {
-    if (req.user)
-    {
-        let date =  "";
+    if (req.user) {
+        let date = "";
         const d = new Date();
-        date = d.getDate().toString() + '/' + (d.getMonth()+1).toString() + '/' + d.getFullYear().toString()
-        console.log("us",req.body)
+        date = d.getDate().toString() + '/' + (d.getMonth() + 1).toString() + '/' + d.getFullYear().toString()
+        console.log("us", req.body)
         await productM.addReivew(req.user.user_id, req.params.product_id, req.body, date);
-        exports.detail(req, res, next);
     }
-    
+
+    exports.detail(req, res, next);
 }
